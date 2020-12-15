5a1e72af52f77b84b188b7e56487b8687e1abf21
Reiko1337 <kurets11@gmail.com>
2020-12-15 23:00:17 +0300

    0.1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+    $('.slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+});
